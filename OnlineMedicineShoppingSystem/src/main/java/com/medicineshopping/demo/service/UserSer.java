06f9ac7a6136921816558e4c7f52702c19332def
Shirdi Sai <64527816+shiridisai-spec@users.noreply.github.com>
2021-04-27 09:19:33 +0530

    Add files via upload

---
+++ OnlineMedicineShoppingSystem/src/main/java/com/medicineshopping/demo/service/UserSer.java
@@ -0,0 +1,24 @@
+package com.medicineshopping.demo.service;
+
+
+import java.util.List;
+
+import com.medicineshopping.demo.entity.User;
+import com.medicineshopping.demo.exceptions.UserNotFoundException;
+
+/**
+ * @author shirdisai
+ *
+ */
+public interface UserSer {
+	
+	User addUser(User user); //Adds users to the databse
+	
+	User updateUser(User user) throws UserNotFoundException;
+
+	void deleteUser(int userId) throws UserNotFoundException;
+		
+	User getUserById(int userId) throws UserNotFoundException; //Gets user infomration by userid
+	
+	List<User> getAllUsers() throws UserNotFoundException;
+}
